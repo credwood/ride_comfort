3017c1630a3348bc42244c323cc4610385af4fb8
Katie Krainc <kkrainc@wilsonihrig.com>
2025-08-06 14:56:00 -0700

    Added Nva, updated Nvd variable names & tried to define triax pairs

--- analysis/metrics.py
+++ analysis/metrics.py
@@ -4,8 +4,11 @@ import numpy as np
 def compute_nmv(Ax_95, Ay_95, Az_95):
     return 6 * np.sqrt(Ax_95**2 + Ay_95**2 + Az_95**2)
 
-def compute_nvd(Ax_50, Ay_50, Az_50, Ay_95):
-    return 3 * np.sqrt(16 * Ax_50**2 + 4 * Ay_50**2 + Az_50**2) + 5 * Ay_95
+def compute_nvd(Axp_50, Ayp_50, Azp_50, Ayp_95):
+    return 3 * np.sqrt(16 * Axp_50**2 + 4 * Ayp_50**2 + Azp_50**2) + 5 * Ayp_95
+
+def compute_nva(Azp_95, Aya_95, Aza_95, Axd_95):
+    return 4 * (Azp_95) + 2 * np.sqrt(Aya_95**2 + Aza_95**2) + 4 * Axd_95
 
 def compute_av(X, Y, Z):
     return np.sqrt(X**2 + Y**2 + Z**2)
