3cb93807d17666d9111b0970b01e14ea7f641b73
Charysse Redwood <Charysse@wia.local>
2025-08-11 12:47:46 -0700

    dataloader and tests for loading all triaxes

--- analysis/loaders.py
+++ analysis/loaders.py
@@ -31,3 +31,46 @@ def load_rave_and_nonrave(folder_path: str):
     z_raw = load_csv(os.path.join(folder_path, raw_files[2]))
 
     return (x_rave, y_rave, z_rave), (x_raw, y_raw, z_raw)
+
+def load_all_triax(dir_path: str):
+    """
+    1. map sensors to triax
+    2. identiify direction of each of the three files mapped to a triax
+    3. identify RAVE and raw versions of each file
+    4. Load each file into a dict (RAVE or raw) with triax 
+    """
+    triaxes_files = {str(num): {"1": [], "2": [], "3": []} for num in range(1, 7)}
+    triaxes_files["5"] = {"4": [], "5": [], "6": []}  # TEAC has 5th triax
+    triax_sensor_map = {"rion_1": "1", "rion_2": "2", "rion_3": "4", "rion_4": "6", "teac": "5"}
+    all_files = os.listdir(dir_path)
+
+    for file in all_files:
+        for sensor, triax in triax_sensor_map.items():
+            if sensor in file.lower():
+                if sensor == 'teac':
+                    if int(file[-5]) > 6:
+                        continue
+                    if int(file[-5]) > 3:
+                        triaxes_files["5"][file[-5]].append(file)
+                    else:
+                        triaxes_files["3"][file[-5]].append(file)
+                else:
+                    if int(file[-5]) > 3:
+                        continue
+                    triaxes_files[triax][file[-5]].append(file)
+
+    triax_data = {str(num): {"1": [], "2": [], "3": []} for num in range(1, 7)}
+    RAVE_triax_data = {str(num): {"1": [], "2": [], "3": []} for num in range(1, 7)}
+    triax_data["5"] = {"4": [], "5": [], "6": []} 
+    RAVE_triax_data["5"] = {"4": [], "5": [], "6": []}
+
+    for triax, dim_files in triaxes_files.items():
+        for dim, files in dim_files.items():
+            for file in files:
+                data_path = os.path.join(dir_path, file)
+                if "RAVE" in file.upper():
+                    RAVE_triax_data[triax][dim] = load_csv(data_path)
+                else:
+                    triax_data[triax][dim] = load_csv(data_path)
+
+    return triax_data, RAVE_triax_data
---
+++ tests/test_dataloader.py
@@ -0,0 +1,52 @@
+from analysis.loaders import load_all_triax
+
+
+def test_load_all_triax():
+    # Assuming the directory 'data/' contains the necessary files for testing
+    non_rave, rave = load_all_triax("data/")
+    assert isinstance(rave, dict)
+    assert isinstance(non_rave, dict)
+    assert len(rave) > 0
+    assert len(non_rave) > 0
+
+    for triax, dim_data in non_rave.items():
+        if triax == "1" or triax == "2":
+            continue
+        for dim, data in dim_data.items():
+            assert data["Description"].iloc[1].split(" ")[1] == triax
+            letter_dim = data["Description"].iloc[2].split(" ")[0]
+            if triax != "5":
+                if letter_dim == "X":
+                    assert dim == "1"
+                elif letter_dim == "Y":
+                    assert dim == "2"
+                elif letter_dim == "Z":
+                    assert dim == "3"
+            else:
+                if letter_dim == "X":
+                    assert dim == "4"
+                elif letter_dim == "Y":
+                    assert dim == "5"
+                elif letter_dim == "Z":
+                    assert dim == "6"
+    
+    for triax, dim_data in rave.items():
+        if triax == "1" or triax == "2":
+            continue
+        for dim, data in dim_data.items():
+            assert data["Description"].iloc[1].split(" ")[1] == triax
+            letter_dim = data["Description"].iloc[2].split(" ")[0]
+            if triax != "5":
+                if letter_dim == "X":
+                    assert dim == "1"
+                elif letter_dim == "Y":
+                    assert dim == "2"
+                elif letter_dim == "Z":
+                    assert dim == "3"
+            else:
+                if letter_dim == "X":
+                    assert dim == "4"
+                elif letter_dim == "Y":
+                    assert dim == "5"
+                elif letter_dim == "Z":
+                    assert dim == "6"
